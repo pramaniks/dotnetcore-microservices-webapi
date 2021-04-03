04ef228b00113a80e2497b52d3e7f9d8f31edf6d
pramaniks <shamsundar.pramanik@radiusww.com>
2021-04-03 05:38:03 -0400

    few changes

---
+++ src/Payment/PaymentManager/PaymentManager/Controllers/PaymentManagerController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PaymentManager.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PaymentManagerController : ControllerBase
+    {
+        [HttpGet]
+        [Route("ping")]
+        public string Ping()
+        {
+            return "Payment Manager service is running OK";
+        }
+    }
+}
